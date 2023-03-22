cc3d72ce0442a00c36574dcb2ebb67516861ead3
Fernando Almeida <fernando@pop-os.localdomain>
2023-03-21 23:06:13 -0300

    fix: ajustando MY_Router.php

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -10,16 +10,16 @@ class MY_Router extends CI_Router {
 	 * Adiciona o REQUEST_METHOD no início do método da classe.
 	 * get_, post_, put_, delete_ ...
 	 */
-	public function set_method($method)
-	{
-		$_verb = strtolower($_SERVER['REQUEST_METHOD']);
-		if(strpos($_verb, 'api/')===0) {
-			$this->method = $_verb.'_'.$method;
-		} else {
-			$this->method = $method;
-		}
-		// die($this->method);
-	}
+	// public function set_method($method)
+	// {
+	// 	$_verb = strtolower($_SERVER['REQUEST_METHOD']);
+	// 	if(strpos($_verb, 'api/')===0) {
+	// 		$this->method = $_verb.'_'.$method;
+	// 	} else {
+	// 		$this->method = $method;
+	// 	}
+	// 	// die($this->method);
+	// }
 
 }
 
